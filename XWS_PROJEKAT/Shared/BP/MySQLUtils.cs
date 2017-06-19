0dd434c7051e64b40ad2705d849e75966726a5e7
djukadjuka <milanns@live.com>
2017-06-19 18:39:00 +0200

    SQL Inicijalno
    Dodata osnovna 'Utility' klasa za SQL stvari. Stavljati sve sto ima veze sa sql-om (konekcioni string, cesti upiti, itd..)

---
+++ XWS_PROJEKAT/Shared/BP/MySQLUtils.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.BP
+{
+	/// <summary>
+	/// Klasa koja sadrzi sve stvarcice za bazu podataka, tipa konekcioni string, korisceni upiti itd..
+	/// </summary>
+	public class MySQLUtils
+	{
+		/// <summary>
+		/// Konekcioni string po standardu za SQL Server Managment Studio SQLEXPRESS. Ako menjas, sacuvaj predhodni kao komentar.
+		/// </summary>
+		public static String StandardConnectionString
+		{
+			//ostavi integrated security na true da ti ne trazi user/pass stalno
+			//menjaj samo XXX iz "Data Source=XXX\\SQLEXPRESS"
+			//XXX ti je ime kompa
+			//Initial Catalog je ime baze, ako se generise druga, mora da se promeni.
+			get{ return "Data Source=DJUKA_PC\\SQLEXPRESS;Initial Catalog=;Integrated Security=True"; }
+		}
+	}
+}
