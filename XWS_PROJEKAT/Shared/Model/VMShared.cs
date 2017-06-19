50fff59277670ce2e6f1ecf829ed1ca27fc774b2
djukadjuka <milanns@live.com>
2017-06-19 18:06:20 +0200

    Dodata abs klasa za MVVM
    Napravljena abstraktna klasa (VMShared) za MVVM koja implementira INotifyPropertyChanged interfejs. Sastavljena kratka dokumentacija za tu klasu cisto da znas gde sta kako sa tim MVVM-om. Ovu klasu neka prosiruje svaki moguci entitet koji cemo prikazivati u View-u. Ubacen Shared namespace koji sadrzi sve klase za modele ili klase koje ce se svuda prosirivati i slicno. U InterfejsiServisa folder stavljati IServiceX klase, a u ModeliProjekta stavljati konkretne modele podataka iz sheme.

---
+++ XWS_PROJEKAT/Shared/Model/VMShared.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Model
+{
+	/// <summary>
+	/// Svaka klasa koja predstavlja model podataka treba ovu klasu da implementira.
+	/// Sadrzi sve stvari bitne za MVVM.
+	/// U svakom propu, pozvati OnPropertyChanged(ime_propertija)
+	/// npr:
+	/// public String Ime{
+	///		get{return ime;}
+	///		set{	ime = value; OnPropertyChanged("Ime");}
+	/// }
+	/// </summary>
+	public abstract class VMShared : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		/// <summary>
+		/// Metoda za MVVM. Pozvati svaki put kada se promeni property. (set{x = value; OnPropertyChanged(...)})
+		/// </summary>
+		/// <param name="propName"> Ime Propertija koji se menja.</param>
+		public void OnPropertyChanged(String propName)
+		{
+			if(PropertyChanged != null)
+			{
+				PropertyChanged(this, new PropertyChangedEventArgs(propName));
+			}
+		}
+	}
+}
