c956e509768bc3ef6f855724c7640a64a22cf81e
djukadjuka <milanns@live.com>
2017-06-25 15:56:41 +0200

    Promenjen naziv Default Namespace-a za XWS_Svc

--- XWS_PROJEKAT/XWS_Svc/Properties/AssemblyInfo.cs
+++ XWS_PROJEKAT/XWS_Svc/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("XWS_Svc")]
+[assembly: AssemblyTitle("XWS")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("XWS_Svc")]
+[assembly: AssemblyProduct("XWS")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
